dea178025316bc4216e14870033149ac224dc018
jeacevedo92 <jeacevedo92@utp.edu.co>
2019-05-23 23:12:30 -0500

    creacion repo refugio animal

---
+++ refugio/apps/adopcion/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Persona(models.Model):
+	nombre = models.CharField(max_length=50)
+	apellidos = models.CharField(max_length=50)
+	edad = models.IntegerField()
+	telefono = models.CharField(max_length=15)
+	email = models.CharField(max_length=50)
+	domicilio = models.CharField(max_length=50)
+
---
+++ refugio/apps/mascota/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from apps.adopcion.models import Persona
+
+class Vacuna(models.Model):
+	nombre = models.CharField(max_length=50)
+		
+
+class Mascota(models.Model):
+	folio = models.CharField(max_length=10,primary_key=True)
+	nombre = models.CharField(max_length=50)
+	sexo = models.CharField(max_length=10)
+	edad = models.IntegerField()
+	fecha_rescate = models.DateField()
+	persona = models.Foreignkey(Persona,null=True,blank=True,on_delete=models.CASCADE)
+	vacuna = models.ManyToManyField(Vacuna)
+
