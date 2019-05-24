9ecefa424a3e8091cf142f36c1c713acd222c1ac
jeacevedo92 <jeacevedo92@utp.edu.co>
2019-05-24 10:51:17 -0500

    models

--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -4,7 +4,6 @@ from apps.adopcion.models import Persona
 
 class Vacuna(models.Model):
 	nombre = models.CharField(max_length=50)
-		
 
 class Mascota(models.Model):
 	folio = models.CharField(max_length=10,primary_key=True)
@@ -12,6 +11,6 @@ class Mascota(models.Model):
 	sexo = models.CharField(max_length=10)
 	edad = models.IntegerField()
 	fecha_rescate = models.DateField()
-	persona = models.Foreignkey(Persona,null=True,blank=True,on_delete=models.CASCADE)
+	persona = models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
 	vacuna = models.ManyToManyField(Vacuna)
 
