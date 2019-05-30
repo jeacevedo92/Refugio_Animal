378884e6e5ff030c47d89b04d4bb2832a0e30ac3
jeacevedo92 <jeacevedo92@utp.edu.co>
2019-05-30 11:07:00 -0500

    forms

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url,include
-from apps.mascota.views import index
+from apps.mascota.views import index,mascota_view
 
 urlpatterns = [
 
-    url('^$', index),
+    url('^$', index, name='index'),
+    url('^nuevo$', mascota_view, name='mascota_crear')
 
 ]
--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from apps.mascota.forms import MascotaForm
 
 def index(request):
     return render(request, 'mascota/index.html')
+
+
+def mascota_view(request):
+    if request.method == 'POST':
+        form = MascotaForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('mascota:')
+    else:
+        form = MascotaForm()
+    return render(request, 'mascota/mascota_form.html', {'form': form})
+
